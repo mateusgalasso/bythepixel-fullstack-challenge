965921661775d794b5e756145cea9333b153105b
Mateus Ezequiel Galasso <mateusgalasso@fbb.org.br>
2023-07-22 10:24:36 -0300

    Add Tailwind CSS and update API test component.
    The changes introduced this commit include the settings and installations necessary to add Tailwind CSS to the project. These were done in the package.json, postcss.config.js, and tailwind.config.js. Tailwind CSS was added to have a more utility-first CSS framework.
    
    There was also heavy refactoring in the ApiTest.vue component to reportedly fetch from an API and display users and their respective weather conditions. A new functionality was added to get weather data based on user location.

--- frontend/src/components/ApiTest.vue
+++ frontend/src/components/ApiTest.vue
@@ -1,31 +1,64 @@
-<script>
-export default {
-  data: () => ({
-    apiResponse: null
-  }),
+<script setup>
+import { ref, onMounted, reactive } from "vue";
 
-  created() {
-    this.fetchData()
-  },
+const apiResponse = ref(null);
+const users = ref([]);
+const userDetails = reactive({ show: false, data: null });
 
-  methods: {
-    async fetchData() {
-      const url = 'http://localhost/'
-      this.apiResponse = await (await fetch(url)).json()
-    }
-  }
+onMounted(async () => {
+  await fetchData();
+});
+
+async function fetchData() {
+  const url = "http://localhost/";
+  apiResponse.value = await (await fetch(url)).json();
+  users.value = apiResponse.value.users;
 }
+
+const fetchDetails = async (user) => {
+  //convert latitude and longitude to user's location decimal with 2 decimal places
+  let lat = parseFloat(user.latitude).toFixed(2);
+  let lon = parseFloat(user.longitude).toFixed(2);
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=7ec19f15335c70699fdee5e6aa1ff011`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=7ec19f15335c70699fdee5e6aa1ff011`;
+  const response = await (await fetch(url)).json();
+  // if code 401, 403, 404, 500, etc. handle it here
+  if (!response.cod || response.cod !== 200) {
+    console.log("Error fetching weather data", response);
+    return;
+  }
+  userDetails.data = {
+    ...user,
+    weather: response.weather[0], // assuming the weather index you want is at 0
+    main: response.main, // additional main weather data
+  };
+  userDetails.show = true;
+};
+const closeDetails = () => {
+  userDetails.show = false;
+};
 </script>
 
 <template>
-  <div v-if="!apiResponse">
-    Pinging the api...
+  <div v-if="!users.length" class="text-center my-5 text-lg font-medium">Fetching users and their current weather...</div>
+
+  <div v-else class="flex flex-col w-1/4 md:w-2/5 lg:w-1/5 ml-5 lg:ml-10">
+    <div v-for="user in users" :key="user.id" class="p-5 flex flex-row items-center justify-between bg-white shadow rounded border border-gray-200 mb-4">
+      <h2 class="text-lg font-medium mb-3">{{ user.name }}</h2>
+      <button @click="fetchDetails(user)" class="px-3 py-2 bg-indigo-500 text-white rounded">Show Details</button>
+    </div>
   </div>
 
-  <div v-if="apiResponse">
-    The api responded with: <br />
-    <code>
-    {{ apiResponse }}
-    </code>
+  <div v-if="userDetails.show" class="fixed inset-0 flex items-center justify-center z-10 p-5">
+    <div class="bg-white rounded shadow-lg w-full max-w-lg mx-auto flex flex-col items-start p-5">
+      <button @click="closeDetails" class="self-end font-bold mb-3">X</button>
+      <h2 class="text-2xl font-bold mb-3">{{ userDetails.data.name }} Detailed Weather:</h2>
+      <div class="text-lg">Main Condition: {{ userDetails.data.weather.main }}</div>
+      <div class="text-lg">Description: {{ userDetails.data.weather.description }}</div>
+      <div class="text-lg">Temperature: {{ userDetails.data.main.temp }} F</div>
+      <div class="text-lg">Humidity: {{ userDetails.data.main.humidity }}%</div>
+      <div class="text-lg">Minimum Temperature: {{ userDetails.data.main.temp_min }} F</div>
+      <div class="text-lg">Maximum Temperature: {{ userDetails.data.main.temp_max }} F</div>
+    </div>
   </div>
 </template>
